a6a6cc4b64acf67f8c5bf946ce297a0918153709
Charlotte Baey <charlotte.baey@math.univ-lille1.fr>
2020-03-28 15:49:20 +0100

    New priors

--- analyses/calibration.R
+++ analyses/calibration.R
@@ -15,21 +15,21 @@ species <- gsub(".rds","",species)
 origin.date = "09-01"
 var.names = list(date="date",plant="plant",session="session",rep="rep",temp="temp.plant",duration="duration")
 temp.params = list(temp.min.cu = -10, temp.max.cu = 15, temp.min.fu = 0, temp.max.fu = 35)
-priors = list(a.cu = prior(distRNG="runif", hyperParams=list(min=0, max=15)),
-              b.cu = prior(distRNG="runif", hyperParams=list(min=2, max=200)),
-              a.fu = prior(distRNG="runif", hyperParams=list(min=5, max=30)),
-              b.fu = prior(distRNG="runif", hyperParams=list(min=0, max=5)),
-              mu = prior(distRNG="rlnorm", hyperParams=list(mean=log(200), sd=1)),
-              s = prior(distRNG="rlnorm", hyperParams=list(mean=log(30), sd=1)))
+priors = list(a.cu = prior(distRNG="rnorm", hyperParams=list(mean=7, sd=3)),
+              b.cu = prior(distRNG="rtruncnorm", hyperParams=list(a=2, mean=100, sd=40)),
+              a.fu = prior(distRNG="rnorm", hyperParams=list(mean=5, sd=4)),
+              b.fu = prior(distRNG="rnorm", hyperParams=list(mean=2.5, sd=1)),
+              mu = prior(distRNG="rnorm", hyperParams=list(mean=500, sd=150)),
+              s = prior(distRNG="rtruncnorm", hyperParams=list(a=0,mean=50, sd=20)))
 temp.data <- readRDS("data/temperaturePlants.rds")
 
-# Aggregate temperatures: one for the day and one for the night
-temp.data$day <- as.Date(temp.data$date) # remove hours
-temp.aggregated <- temp.data %>% dplyr::group_by(day) %>% dplyr::mutate(temp.day = min(temp.plant), temp.night = max(temp.plant)) # compute min and max per day
-temp.data <- temp.aggregated %>% dplyr::select(-date,-temp.out,-temp.in,-temp.plant) %>% dplyr::distinct() # drop variables at the 3-hour level
-
-# create a unique column from cols "temp.day" and "temp.night"
-temp.data <- tidyr::pivot_longer(temp.data, cols = tidyr::starts_with("temp"), values_to = "temp.plant") %>% dplyr::select(-name)
+# # Aggregate temperatures: one for the day and one for the night
+# temp.data$day <- as.Date(temp.data$date) # remove hours
+# temp.aggregated <- temp.data %>% dplyr::group_by(day) %>% dplyr::mutate(temp.day = min(temp.plant), temp.night = max(temp.plant)) # compute min and max per day
+# temp.data <- temp.aggregated %>% dplyr::select(-date,-temp.out,-temp.in,-temp.plant) %>% dplyr::distinct() # drop variables at the 3-hour level
+#
+# # create a unique column from cols "temp.day" and "temp.night"
+# temp.data <- tidyr::pivot_longer(temp.data, cols = tidyr::starts_with("temp"), values_to = "temp.plant") %>% dplyr::select(-name)
 
 # Add species in temp.data
 temp.data$species <- gsub('[0-9]+', '', temp.data$plant)
@@ -41,6 +41,7 @@ obs.data <- readRDS(paste0("data/",files[i]))
 
 calib <- mcmc(data = list(obs.data=obs.data,
                           temp.data=temp.data,
+                          priors=priors,
                           var.names=var.names),
               temp.params = temp.params,
               control = list(proposal="AdGl",
